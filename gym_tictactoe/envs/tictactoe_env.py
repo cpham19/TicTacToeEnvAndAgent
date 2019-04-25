42fdb8b3a01a4f8ce514a8e62638e6dc6e105020
Calvin Pham <calvinthepham@gmail.com>
2019-04-24 17:41:25 -0700

    a working tictactoe environment with a q-learning bot?

--- gym_tictactoe/envs/tictactoe_env.py
+++ gym_tictactoe/envs/tictactoe_env.py
@@ -24,39 +24,23 @@ class PlayerAgent:
 		move = random.sample(numbers, 1)[0]
 		return move
 
-class TicTacToeAgent:
+class TicTacToeAgent(PlayerAgent):
 	def __init__(self, mark):
+		super().__init__(mark)
 		self.learning_rate = 0.5
 		self.discount_factor = 0.01
 		self.exploration_rate = 0.33
-		self.mark = mark
 
+		self.q_states = {}
 		self.state_order = []
 
-	@property
-	def mark(self):
-		return self.__mark
+		self.mark = mark
 
-	@mark.setter
-	def mark(self, mark):
-		self.__mark = mark
-
-	# Random move from bot
-	def action(self, state):
-		numbers = []
-		array = np.array(state).ravel()
-		for i in range(0, 9):
-			if (array[i] == ' '):
-				numbers.append(i)
-
-		move = random.sample(numbers, 1)[0]
-		return move
-
-	# Transcode the state matrix into a string
+	# Transcode the state matrix into a string (used for recording the state before making the winning move)
 	def serializeState(self, state):
 		string = ""
-		for row in range(0, len(self.state)):
-			for col in range(0, len(self.state[row])):
+		for row in range(0, len(state)):
+			for col in range(0, len(state[row])):
 				if(state[row][col] == 'O'):
 					string += '1'
 				elif(state[row][col] == 'X'):
@@ -65,6 +49,55 @@ class TicTacToeAgent:
 					string += '0'
 
 		return string
+
+	# Determine the move for bot
+	def action(self, state):
+		# One dimensionl array of the current state of the game
+		current_state = np.array(state).ravel()
+
+		# State key for recording movements
+		state_key = self.serializeState(state)
+
+		# Determining if the bot explores or not
+		exploration = np.random.random() < self.exploration_rate
+
+		possible_moves = []
+
+		# Checking if the board only has one move left
+		for i in range(0, 9):
+			if (current_state[i] == " "):
+				possible_moves.append(i)
+
+		if (len(possible_moves) == 1):
+			move = possible_moves[0]
+			self.state_order.append((state_key, move))
+			return move
+
+
+		possible_moves = []
+		# For Exploiting: If the bot remembers the current state, it can make a calculated move
+		if (not exploration and state_key in self.q_states):
+			print("EXPLOITING")
+			# Get the q values of the currnt state (these are the numbers that are constantly changing after rewarding and punishing the bot)
+			state_values = self.q_states[state_key].ravel()
+			print(state_values)
+
+			max_reward = np.max(state_values)
+			for i in range(0, 9):
+				if (state_values[i] == max_reward):
+					possible_moves.append(i)
+
+
+		# For Exploring: Making a random move based on the current state
+		else:
+			print("EXPLORING")
+			for i in range(0, 9):
+				if (current_state[i] == ' '):
+					possible_moves.append(i)
+
+		move = random.choice(possible_moves)
+		self.state_order.append((state_key, move))
+		return move
 
 
 class TicTacToe(gym.Env):
@@ -112,7 +145,6 @@ class TicTacToe(gym.Env):
 
 
 	def __init__(self):
-		self.games = 100
 		self.playerWins = 0
 		self.draws = 0
 		self.cpuWins = 0
@@ -179,55 +211,95 @@ class TicTacToe(gym.Env):
 	def step(self, target, mark):
 		stateObj = {}
 
+		# Pointless if you and the bot knows how to make proper moves!!! (for example, prevent the player from pressing a square on a GUI or rerandomizing a random move from a bot if it's invalid)
 		if self.state[int(target/3)][target%3] != " ":
 			print("Invalid Step")
-			return [self.state, self.reward, self.done, self.add]
+			self.validMove = False
+
 		else:
 			self.state[int(target/3)][target%3] = mark
-
+			self.validMove = True
 			self.turns += 1
 			if(self.turns == 9):
 				self.done = 1;
 
 		checkState = self.check()
 
-		# If Someone wins
+		# Reward Bot for winning/Punish Bot for losing
 		if(checkState['win'] == True):
-			self.games += 1
 			self.done = 1;
 
 			if (checkState['winner'] == "Player"):
 				print("Player wins.", sep="", end="\n")
-				self.add[0] = 1;
-				self.reward = -100
+				self.rewardBot(-1)
 				self.playerWins += 1
 			else:
 				print("CPU wins.", sep="", end="\n")
-				self.add[1] = 1;
-				self.reward = 100
+				self.rewardBot(1)
 				self.cpuWins += 1
 
 		# If the game's outcome is draw
 		elif (checkState['win'] == False and self.done == 1):
 			self.draws += 1
+			self.rewardBot(0.5)
 			print("DRAW! No one wins!")
 
 		stateObj['state'] = self.state
-		stateObj['reward'] = self.reward
 		stateObj['done'] = self.done
-		stateObj['add'] = self.add
+		stateObj['winner'] = checkState['winner']
+		stateObj['validMove'] = self.validMove
 
 		return stateObj
 
-	# Reset the board and counters
+	# Rewarding or Punishing the bot
+	def rewardBot(self, reward):
+		# Last move made by bot
+		last_state_key, last_move = self.cpu.state_order.pop()
+
+		# Zero matrix
+		self.cpu.q_states[last_state_key] = np.zeros((3, 3))
+		# Rewarding the state
+		self.cpu.q_states[last_state_key].itemset(last_move, reward)
+
+		# Going through the rest of the moves that the bot has made
+		while (self.cpu.state_order):
+			# Move made by bot
+			state_key, move = self.cpu.state_order.pop()
+
+			# Reducing reward
+			reward *= self.cpu.discount_factor
+
+			# Calculating temporal difference
+			old_state = self.cpu.q_states.get(state_key, np.zeros((3, 3)))
+			temporal_difference = self.cpu.learning_rate * ((reward * self.cpu.q_states[last_state_key]) - old_state)
+
+			# State was encountered before so we increase the reward
+			if (state_key in self.cpu.q_states):
+				reward += temporal_difference.item(last_move)
+				self.cpu.q_states[state_key].itemset(move, reward)
+
+			# State was not encountered before so we set the reward to a new one
+			else:
+				# Assign a new key to the array of states
+				self.cpu.q_states[state_key] = np.zeros((3,3))
+				reward = temporal_difference.item(last_move)
+				self.cpu.q_states[state_key].itemset(move, reward)
+
+			# Last state key and move are now the previous state key and move (as we pop moves out of the state_order array)
+			last_state_key = state_key
+			last_move = move
+
+
+
+	#Reset the board and counters
 	def reset(self):
 		# Randomize the player who goes first
-		self.turn = 'Player' if (random.randint(0, 1) == 0) else 'CPU'
+		#self.turn = 'Player' if (random.randint(0, 1) == 0) else 'CPU'
+		self.turn = 'CPU'
 		self.state = [[' ', ' ', ' '],[' ', ' ', ' '],[' ', ' ', ' ']]
 		self.turns = 0
 		self.done = 0
-		self.add = [0, 0]
-		self.reward = 0
+		self.validMove = True
 
 		return self.state
 
@@ -243,30 +315,67 @@ class TicTacToe(gym.Env):
 				print('\n---------')
 		print("\n")
 
+	# Train the bot
+	def train(self):
+		self.player = PlayerAgent('O')
+		self.cpu = TicTacToeAgent('X')
+
+		for i in range(1,1000):
+			print("Game #" + str(i) + "\n")
+			self.reset()
+
+			while (True):
+				stateObj = None
+				action = self.cpu.action(self.state)
+				stateObj = self.step(action, self.cpu.mark)
+				print(stateObj)
+				self.render()
+
+				if (stateObj['done'] == 1):
+					print("Q_STATES\n--------")
+					print(self.cpu.q_states)
+					print("\n--------------------------------------------------------------------------------------")
+
+					print("Game #" + str(i) + " done!\n------------------------------------------------------------------------------------------------------\n")
+					break
+
 	# Play Tic Tac Toe game for a specified amount
 	def play(self):
 		self.player = PlayerAgent('O')
 		self.cpu = TicTacToeAgent('X')
 
-		for i in range(1, 101):
+		for i in range(1,10000):
 			print("Game #" + str(i) + "\n")
 			self.reset()
 
 			while (True):
 				stateObj = None
 				if(self.turn == "Player"):
+					print("Player's Turn\n-------------")
 					action = self.player.action(self.state)
-					mark = self.player.mark
-					stateObj = self.step(action, mark)
+					print("Player selects " + str(action))
+					stateObj = self.step(action, self.player.mark)
 					self.turn = 'CPU'
-				else:
-					action = self.cpu.action(self.state)
-					mark = self.cpu.mark
-					stateObj = self.step(action, mark)
-					self.turn = 'Player'
+					print(stateObj)
+					self.render()
 
-				print(stateObj)
-				self.render()
+				else:
+					print("CPU's Turn\n-------------")
+					action = self.cpu.action(self.state)
+					print("CPU selects " + str(action))
+					stateObj = self.step(action, self.cpu.mark)
+					print(stateObj)
+					self.render()
+
+					while(stateObj['validMove'] != True):
+						print("CPU's Turn\n-------------")
+						action = self.cpu.action(self.state)
+						print("CPU selects " + str(action))
+						stateObj = self.step(action, self.cpu.mark)
+						print(stateObj)
+						self.render()
+
+					self.turn = 'Player'
 
 				if (stateObj['done'] == 1):
 					print("Game #" + str(i) + " done!\n------------------------------------------------------------------------------------------------------\n")
--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,16 @@
-# Code inspired by apoddar573 at https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa and https://github.com/apoddar573/Tic-Tac-Toe-Gym_Environment/tree/master/gym-tictac4/gym_tictac4
+# Code inspired by ...
+# apoddar573 at https://medium.com/@apoddar573/making-your-own-custom-environment-in-gym-c3b65ff8cdaa and https://github.com/apoddar573/Tic-Tac-Toe-Gym_Environment/
+# giladariel at https://towardsdatascience.com/reinforcement-learning-and-deep-reinforcement-learning-with-tic-tac-toe-588d09c41dda and https://github.com/giladariel/TicTacToe_RL/
+# AmreshVenugopal at https://github.com/AmreshVenugopal/tic_tac_toe
+
 import gym
 import gym_tictactoe
 
 def main():
     env = gym.make('tictactoe-v0')
     env.play()
+
+    #env.train()
 
 
 if __name__ == '__main__':
